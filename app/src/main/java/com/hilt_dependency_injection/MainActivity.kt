c66a358671f153fc483bc2ad1ba475ade2de632d
zeeshanayaz <zeeshanayaz1@gmail.com>
2021-04-12 06:35:11 -0700

    working

--- app/src/main/java/com/hilt_dependency_injection/MainActivity.kt
+++ app/src/main/java/com/hilt_dependency_injection/MainActivity.kt
@@ -2,10 +2,27 @@ package com.hilt_dependency_injection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(someClass.doAThing())
+    }
+}
+
+class SomeClass
+@Inject
+constructor(){
+    fun doAThing():String{
+        return  "Look I did a thing"
     }
 }
